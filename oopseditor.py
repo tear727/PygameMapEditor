1225c247b8d0f05dd1cb4998e870e043153c17ef
Tren Esten Alexander Resh <TrentResh@Trens-MBP.zyxel.com>
2015-11-12 11:53:17 -0800

    save feature

--- oopseditor.py
+++ oopseditor.py
@@ -1,5 +1,6 @@
 import pygame
 import pickle
+import os
 
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 600
@@ -34,7 +35,7 @@ class Graph(object):
         return self.grid
 
 class Tile(object):
-    def __init__(self, width=None, height=None, color=None, loadedimage=None):
+    def __init__(self, width=None, height=None, solid=None, color=None, loadedimage=None):
         self.width = width
         self.height = height
         self.color = color
@@ -45,55 +46,91 @@ class Tile(object):
         if self.loadedimage != None:
             self.image = pygame.image.load(str(loadedimage))
             self.rect = self.image.get_rect()
-            self.width = self.rect.x
-            self.height = self.rect.y
+            self.width = self.rect.width
+            self.height = self.rect.height
         self.image.convert()
         self.rect = self.image.get_rect()
+        self.solid = solid
 
+def paused(pause):
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_o:
+                    pause = False
+        clock.tick(60)
 
 graph = Graph(rows=60, columns=80)
 graph.make_graph()
 
+def save(arg):
+    with open("savegame", "wb") as f:
+        pickle.dump(arg, f)
+        print "file saved"
 
-# with open("savegame", "rb") as f:
-#     graph = pickle.load(f)
+def delete_save():
+    answer = raw_input("Are you sure you want to delete the save file? y/n\n").lower()
+    if answer == "y":
+        os.remove("savegame")
+    elif answer == "n":
+        print "resuming"
+    else:
+        print "choose y or n"
 
+if os.path.exists("savegame"):
+    with open("savegame", "rb") as f:
+        graph = pickle.load(f)
+screen.fill(WHITE)
 
 while not game_exit:
     for row in range(graph.rows):
         for column in range(graph.columns):
             if graph.grid[row][column] == 0:
                 color = WHITE
-                tile = Tile(width=20, height=20, color=color)
+                tile = Tile(width=50, height=50, color=color)
             elif graph.grid[row][column] == 1:
-                color = None
-                tile = Tile(width=20, height=20, color=None, loadedimage="poro.png")
+                color = PURPLE
+                tile = Tile(width=50, height=50, color=None, loadedimage="poro.png")
                 # print tile.rect.x, tile.rect.y
             elif graph.grid[row][column] == 2:
                 color = BLUE
-                tile = Tile(width=20, height=20, color=color)
+                tile = Tile(width=50, height=50, color=color)
             elif graph.grid[row][column] == 3:
                 color = GREEN
-                tile = Tile(width=20, height=20, color=color)
+                tile = Tile(width=50, height=50, color=color)
             elif graph.grid[row][column] == 4:
                 color = BROWN
-                tile = Tile(width=20, height=20, color=color)
+                tile = Tile(width=50, height=50, color=color)
             elif graph.grid[row][column] == 5:
                 color = YELLOW
-                tile = Tile(width=20, height=20, color=color)
+                tile = Tile(width=50, height=50, color=color)
             elif graph.grid[row][column] == 6:
                 color = BLACK
-                tile = Tile(width=20, height=20, color=color)
+                tile = Tile(width=50, height=50, color=color)
             screen.blit(tile.image, ((tile.width*column),(tile.height*row)))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_exit = True
+            prompt = raw_input("Exit? Unsaved chaned will be lost.\ny/n\n").lower()
+            if prompt == "y":
+                game_exit = True
+            elif prompt == "n":
+                print "resuming"
 
         if event.type == pygame.KEYDOWN:
             pos = pygame.mouse.get_pos()
             column = pos[0] // tile.width
             row = pos[1] // tile.height
+            if event.key == pygame.K_p:
+                pause = True
+                paused(pause)
+            if event.key == pygame.K_t:
+                save(graph)
+            if event.key == pygame.K_BACKSPACE:
+                delete_save()
+
             if event.key == pygame.K_a:
                 graph.grid[row][column] = 1
                 print "Position: {0}, Grid Coordinates: {1}, {2}".format(
@@ -122,8 +159,4 @@ while not game_exit:
     clock.tick(60)
     pygame.display.flip()
 
-# with open("savegame", "wb") as f:
-#     pickle.dump(graph, f)
-
-print graph.grid
 pygame.quit()
