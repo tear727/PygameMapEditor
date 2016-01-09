8410a9c67def0018c3f11ef2162551f0d0b9a01e
iminurnamez <easinerf@gmail.com>
2016-01-09 13:46:42 -0500

    some suggested changes

--- oopseditorrev.py
+++ oopseditorrev.py
@@ -23,8 +23,8 @@ pygame.init()
 size = (DISPLAY_WIDTH, DISPLAY_WIDTH)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-game_exit = False
 
+FONT = pygame.font.Font(None, 32)
 
 class Entity(pygame.sprite.Sprite):
 	"""Base Entity class that provides Sprite class inheritance for subclasses"""
@@ -35,9 +35,18 @@ class Entity(pygame.sprite.Sprite):
 class Controller(object):
     def __init__(self, name):
         self.name = name
-        self.mouse_pos = None
-        self.mouse_x = 0
-        self.mouse_y = 0
+        #these will keep track of how far the graph has moved overall
+        self.x_offset, self.y_offset = 0, 0
+        #these will hold the current mouse position relative to the graph
+        self.graph_x, self.graph_y = 0, 0
+        self.terrain_keys = {
+                pygame.K_a: 1,
+                pygame.K_s: 2,
+                pygame.K_d: 3,
+                pygame.K_f: 4,
+                pygame.K_g: 5,
+                pygame.K_h: 6}
+        self.game_exit = False
 
     def __str__(self):
         return unicode(self.name)
@@ -47,28 +56,35 @@ class Controller(object):
         self.mouse()
 
     def mouse(self):
-        self.mouse_pos = pygame.mouse.get_pos()
-        self.mouse_x = self.mouse_pos[0]
-        self.mouse_y = self.mouse_pos[1]
-
-    def shift_mouse(self, shift_x, shift_y):
-        "Handles mouse shift"
-        self.mouse_x += shift_x
-        self.mouse_y += shift_y
-        print self.mouse_x, self.mouse_y
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        offset = [0, 0]
+        if mouse_x > 770:
+            offset[0] = -5 #700 - mouse_x
+        elif mouse_x < 30:
+            offset[0] = 5 #30 - mouse_x
+        if mouse_y > 770:
+            offset[1] = -5 #700 - mouse_y
+        elif mouse_y < 30:
+            offset[1] = 5 #30 - mouse_y
+        graph.shift_graph(*offset)
+        self.x_offset -= offset[0]
+        self.y_offset -= offset[1]
+        self.graph_x = mouse_x + self.x_offset
+        self.graph_y = mouse_y + self.y_offset
 
     def keyboard(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                prompt = raw_input("Exit? Unsaved chaned will be lost.\ny/n\n").lower()
-                if prompt == "y":
-                    game_exit = True
-                elif prompt == "n":
+                prompt = "Exit? Unsaved chaned will be lost. y/n"
+                ok = confirmation_prompt(prompt)
+                if ok:
+                    self.game_exit = True
+                else:
                     print "resuming"
 
             if event.type == pygame.KEYDOWN:
-                column = self.mouse_x // TILE_HEIGHT
-                row = self.mouse_y // TILE_WIDTH
+                column = self.graph_x // TILE_HEIGHT
+                row = self.graph_y // TILE_WIDTH
                 if event.key == pygame.K_p:
                     pause = True
                     paused(pause)
@@ -76,32 +92,18 @@ class Controller(object):
                     save(graph)
                 if event.key == pygame.K_BACKSPACE:
                     delete_save()
-
-                if event.key == pygame.K_a:
-                    graph.grid[row][column] = 1
+                #check if current cell is in grid first (careful of negative indices, they're valid but incorrect)
+                bad_column = not (0 <= column <= graph.columns - 1)
+                bad_row = not (0 <= row <= graph.rows - 1)
+                if bad_column or bad_row:
+                    return
+                try:
+                    terrain_num = self.terrain_keys[event.key]
+                    graph.grid[column][row] = terrain_num
                     print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-                    self.mouse_pos, row,column)
-                if event.key == pygame.K_s:
-                    graph.grid[row][column] = 2
-                    print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-                    self.mouse_pos, row,column)
-                if event.key == pygame.K_d:
-                    graph.grid[row][column] = 3
-                    print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-                    self.mouse_pos, row,column)
-                if event.key == pygame.K_f:
-                    graph.grid[row][column] = 4
-                    print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-                    self.mouse_pos, row,column)
-                if event.key == pygame.K_g:
-                    graph.grid[row][column] = 5
-                    print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-                    self.mouse_pos, row,column)
-                if event.key == pygame.K_h:
-                    graph.grid[row][column] = 6
-                    print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-                    self.mouse_pos, row,column)
-
+                            pygame.mouse.get_pos(), column, row)
+                except KeyError:
+                    pass
 
 
 class Graph(object):
@@ -111,39 +113,30 @@ class Graph(object):
         self.grid = []
         self.graph_shift_x = 0
         self.graph_shift_y = 0
+        self.terrain_colors = {
+                0: WHITE,
+                1: PURPLE,
+                2: BLUE,
+                3: GREEN,
+                4: BROWN,
+                5: YELLOW,
+                6: BLACK}
 
     def graph_update(self):
-        for row in range(graph.rows):
-            for column in range(graph.columns):
-                if self.grid[row][column] == 0:
-                    color = WHITE
-                    tile = Tile(width=TILE_WIDTH, height=TILE_HEIGHT, color=color)
-                elif self.grid[row][column] == 1:
-                    color = PURPLE
-                    tile = Tile(width=TILE_WIDTH, height=TILE_HEIGHT, color=None, loadedimage="poro.png")
-                    # print tile.rect.x, tile.rect.y
-                elif self.grid[row][column] == 2:
-                    color = BLUE
-                    tile = Tile(width=TILE_WIDTH, height=TILE_HEIGHT, color=color)
-                elif self.grid[row][column] == 3:
-                    color = GREEN
-                    tile = Tile(width=TILE_WIDTH, height=TILE_HEIGHT, color=color)
-                elif self.grid[row][column] == 4:
-                    color = BROWN
-                    tile = Tile(width=TILE_WIDTH, height=TILE_HEIGHT, color=color)
-                elif self.grid[row][column] == 5:
-                    color = YELLOW
-                    tile = Tile(width=TILE_WIDTH, height=TILE_HEIGHT, color=color)
-                elif self.grid[row][column] == 6:
-                    color = BLACK
-                    tile = Tile(width=TILE_WIDTH, height=TILE_HEIGHT, color=color)
+        for column in range(self.columns):
+            for row in range(self.rows):
+                color = self.terrain_colors[self.grid[column][row]]
+                tile = Tile(width=TILE_WIDTH, height=TILE_HEIGHT, color=color)
                 screen.blit(tile.image, ((TILE_WIDTH*column)+self.graph_shift_x,(TILE_HEIGHT*row)+self.graph_shift_y))
 
     def make_graph(self):
-        for rows in range(self.rows):
-            self.grid.append([])
-            for columns in range(self.columns):
-                self.grid[rows].append(0)
+        for x in range(self.columns):
+            self.grid.append([0] * self.rows)
+
+        #for rows in range(self.rows):
+        #    self.grid.append([])
+        #    for columns in range(self.columns):
+        #        self.grid[rows].append(0)
         return self.grid
 
     def shift_graph(self, shift_x, shift_y):
@@ -183,19 +176,31 @@ def paused(pause):
         clock.tick(60)
 
 
+def confirmation_prompt(prompt_text):
+    msg = FONT.render(prompt_text, True, pygame.Color("gray80"), pygame.Color("gray20"))
+    msg_rect = msg.get_rect(center=(DISPLAY_WIDTH//2, DISPLAY_HEIGHT//2))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_y:
+                    return True
+                else:
+                    return False
+        screen.blit(msg, msg_rect)
+        pygame.display.update()
+
 def save(arg):
     with open("savegame", "wb") as f:
         cPickle.dump(arg, f)
         print "file saved"
 
 def delete_save():
-    answer = raw_input("Are you sure you want to delete the save file? y/n\n").lower()
-    if answer == "y":
+    prompt = "Are you sure you want to delete the save file? y/n\n"
+    ok = confirmation_prompt(prompt)
+    if ok:
         os.remove("savegame")
-    elif answer == "n":
-        print "resuming"
     else:
-        print "choose y or n"
+        print "resuming"
 
 try:
 	if os.path.exists("savegame"):
@@ -212,107 +217,10 @@ graph.make_graph()
 print graph.graph_shift_x
 print graph.grid
 
-while not game_exit:
+while not controller.game_exit:
     screen.fill(WHITE)
     graph.graph_update()
     controller.update_controller()
-
-    if controller.mouse_x > 700:
-        diff = controller.mouse_x - 700
-        #print diff, "mouse_x greater than 700"
-        graph.shift_graph(-diff, 0)
-        #controller.shift_mouse(-diff, 0)
-    if controller.mouse_x < 30:
-        diff = 30 - controller.mouse_x
-        #print diff, "mouse_x less than 30"
-        graph.shift_graph(diff, 0)
-        # controller.shift_mouse(diff, 0)
-    if controller.mouse_y > 700:
-        diff = controller.mouse_y - 700
-        #print diff, "mouse_y greater than 700"
-        graph.shift_graph(0, -diff)
-        # controller.shift_mouse(0, -diff)
-    if controller.mouse_y < 30:
-        diff = 30 - controller.mouse_y
-        #print diff, "mouse_y less than 30"
-        graph.shift_graph(0, diff)
-        # controller.shift_mouse(0, diff)
-
-
-
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         prompt = raw_input("Exit? Unsaved chaned will be lost.\ny/n\n").lower()
-    #         if prompt == "y":
-    #             game_exit = True
-    #         elif prompt == "n":
-    #             print "resuming"
-    #
-    #     if event.type == pygame.KEYDOWN:
-    #         pos = pygame.mouse.get_pos()
-    #         column = pos[0] // tile.width
-    #         row = pos[1] // tile.height
-    #         if event.key == pygame.K_p:
-    #             pause = True
-    #             paused(pause)
-    #         if event.key == pygame.K_t:
-    #             save(graph)
-    #         if event.key == pygame.K_BACKSPACE:
-    #             delete_save()
-    #
-    #         if event.key == pygame.K_a:
-    #             graph.grid[row][column] = 1
-    #             print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-    #             pos, row,column)
-    #         if event.key == pygame.K_s:
-    #             graph.grid[row][column] = 2
-    #             print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-    #             pos, row,column)
-    #         if event.key == pygame.K_d:
-    #             graph.grid[row][column] = 3
-    #             print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-    #             pos, row,column)
-    #         if event.key == pygame.K_f:
-    #             graph.grid[row][column] = 4
-    #             print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-    #             pos, row,column)
-    #         if event.key == pygame.K_g:
-    #             graph.grid[row][column] = 5
-    #             print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-    #             pos, row,column)
-    #         if event.key == pygame.K_h:
-    #             graph.grid[row][column] = 6
-    #             print "Position: {0}, Grid Coordinates: {1}, {2}".format(
-    #             pos, row,column)
-    #
-    # for row in range(graph.rows):
-    #     for column in range(graph.columns):
-    #         if graph.grid[row][column] == 0:
-    #             color = WHITE
-    #             tile = Tile(width=50, height=50, color=color)
-    #         elif graph.grid[row][column] == 1:
-    #             color = PURPLE
-    #             tile = Tile(width=50, height=50, color=None, loadedimage="poro.png")
-    #             # print tile.rect.x, tile.rect.y
-    #         elif graph.grid[row][column] == 2:
-    #             color = BLUE
-    #             tile = Tile(width=50, height=50, color=color)
-    #         elif graph.grid[row][column] == 3:
-    #             color = GREEN
-    #             tile = Tile(width=50, height=50, color=color)
-    #         elif graph.grid[row][column] == 4:
-    #             color = BROWN
-    #             tile = Tile(width=50, height=50, color=color)
-    #         elif graph.grid[row][column] == 5:
-    #             color = YELLOW
-    #             tile = Tile(width=50, height=50, color=color)
-    #         elif graph.grid[row][column] == 6:
-    #             color = BLACK
-    #             tile = Tile(width=50, height=50, color=color)
-    #         screen.blit(tile.image, ((tile.width*column),(tile.height*row)))
-    #
-    # print graph.graph_shift_x, graph.graph_shift_y
-
     clock.tick(60)
     pygame.display.flip()
 
