75857504725dc333feaaaa2bcce39e5bb5755d05
Trent Resh <tear728@gmail.com>
2019-09-12 12:36:41 -0700

    minimap

--- oopseditorrev.py
+++ oopseditorrev.py
@@ -14,6 +14,8 @@ GREEN = (34,139,34)
 YELLOW = (255,255,51)
 BROWN = (210,180,140)
 BLUE = (0, 255, 255)
+PORO = "poro.png"
+
 
 TILE_WIDTH = 50
 TILE_HEIGHT = 50
@@ -23,6 +25,8 @@ pygame.init()
 size = (DISPLAY_WIDTH, DISPLAY_WIDTH)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+background = pygame.Surface((200, 150))
+pygame.draw.rect(background,(0,255,255),(0,0,200,200))
 
 FONT = pygame.font.Font(None, 32)
 
@@ -45,7 +49,8 @@ class Controller(object):
                 pygame.K_d: 3,
                 pygame.K_f: 4,
                 pygame.K_g: 5,
-                pygame.K_h: 6}
+                pygame.K_h: 6,
+                pygame.K_j: 7}
         self.game_exit = False
 
     def __str__(self):
@@ -59,13 +64,13 @@ class Controller(object):
         mouse_x, mouse_y = pygame.mouse.get_pos()
         offset = [0, 0]
         if mouse_x > 770:
-            offset[0] = -5 #700 - mouse_x
+            offset[0] = -75 #700 - mouse_x
         elif mouse_x < 30:
-            offset[0] = 5 #30 - mouse_x
+            offset[0] = 75 #30 - mouse_x
         if mouse_y > 770:
-            offset[1] = -5 #700 - mouse_y
+            offset[1] = -75 #700 - mouse_y
         elif mouse_y < 30:
-            offset[1] = 5 #30 - mouse_y
+            offset[1] = 75 #30 - mouse_y
         graph.shift_graph(*offset)
         self.x_offset -= offset[0]
         self.y_offset -= offset[1]
@@ -107,10 +112,13 @@ class Controller(object):
 
 
 class Graph(object):
-    def __init__(self, rows=None, columns=None):
+    def __init__(self, rows=None, columns=None, scale_factor=None, grid=None):
         self.rows = rows
         self.columns = columns
-        self.grid = []
+        if grid != None:
+            self.grid = grid
+        else:
+            self.grid = []
         self.graph_shift_x = 0
         self.graph_shift_y = 0
         self.terrain_colors = {
@@ -120,29 +128,37 @@ class Graph(object):
                 3: GREEN,
                 4: BROWN,
                 5: YELLOW,
-                6: BLACK}
+                6: BLACK,
+                7: PORO 
+        }
+        self.scale_factor = scale_factor    
 
-    def graph_update(self):
+    def set_scale_factor(self, scale_factor=None):
+        self.scale_factor = scale_factor
+
+    def graph_update(self, minimap=None):
         for column in range(self.columns):
             for row in range(self.rows):
                 color = self.terrain_colors[self.grid[column][row]]
-                tile = Tile(width=TILE_WIDTH, height=TILE_HEIGHT, color=color)
-                screen.blit(tile.image, ((TILE_WIDTH*column)+self.graph_shift_x,(TILE_HEIGHT*row)+self.graph_shift_y))
+                if type(color) == str:
+                    tile = Tile(width=(TILE_WIDTH), height=(TILE_HEIGHT), loadedimage=color)
+                else:
+                    tile = Tile(width=(TILE_WIDTH), height=(TILE_HEIGHT), color=color)
+                screen.blit(tile.image, (((TILE_WIDTH)*column)+self.graph_shift_x,((TILE_HEIGHT)*row)+self.graph_shift_y))
+
 
     def make_graph(self):
         for x in range(self.columns):
             self.grid.append([0] * self.rows)
-
-        #for rows in range(self.rows):
-        #    self.grid.append([])
-        #    for columns in range(self.columns):
-        #        self.grid[rows].append(0)
         return self.grid
 
     def shift_graph(self, shift_x, shift_y):
         "Shifts graph"
         self.graph_shift_x += shift_x
         self.graph_shift_y += shift_y
+
+    def copy(self):
+        return self.grid
 
 
 class Tile(Entity):
@@ -156,7 +172,7 @@ class Tile(Entity):
             self.image.fill(self.color)
         self.loadedimage = loadedimage
         if self.loadedimage != None:
-            self.image = pygame.image.load(str(loadedimage))
+            self.image = pygame.image.load(loadedimage)
             self.rect = self.image.get_rect()
             self.width = self.rect.width
             self.height = self.rect.height
@@ -164,6 +180,38 @@ class Tile(Entity):
         self.rect = self.image.get_rect()
         self.solid = solid
 
+
+class MiniMap(object):
+    def __init__(self, width=None, height=None, graph=None):
+        self.width = width
+        self.height = height
+        self.graph = graph
+        self.grid = graph.copy()
+        self.pos = (200, 150)
+        self.terrain_colors = {
+                0: WHITE,
+                1: PURPLE,
+                2: BLUE,
+                3: GREEN,
+                4: BROWN,
+                5: YELLOW,
+                6: BLACK,
+                7: PORO 
+        }
+
+    def scale_graph(self, scale_factor=None):
+        if graph != None:
+            self.graph.set_scale_factor(scale_factor)
+
+    def minimap_update(self, graph=None):
+        self.grid = graph.copy()
+        print self.grid
+        # for column in range(80):
+        #     for row in range(60):
+        #         color = self.terrain_colors[self.grid[column][row]]
+        #         print "color: ", color
+        #         pygame.draw.rect(background, color, (row,column,row,column))
+        screen.blit(background, (0,0))
 
 def paused(pause):
     while pause:
@@ -174,7 +222,6 @@ def paused(pause):
                 if event.key == pygame.K_o:
                     pause = False
         clock.tick(60)
-
 
 def confirmation_prompt(prompt_text):
     msg = FONT.render(prompt_text, True, pygame.Color("gray80"), pygame.Color("gray20"))
@@ -212,14 +259,16 @@ except:
 """Instantiations"""
 
 controller = Controller("controller1")
-graph = Graph(rows=60, columns=80)
+graph = Graph(rows=60, columns=80, scale_factor=4)
 graph.make_graph()
 print graph.graph_shift_x
 print graph.grid
+mini_map = MiniMap(graph=graph)
 
 while not controller.game_exit:
     screen.fill(WHITE)
-    graph.graph_update()
+    graph.graph_update(minimap=background)
+    mini_map.minimap_update(graph)
     controller.update_controller()
     clock.tick(60)
     pygame.display.flip()
